06deca32da18b645d60505e17c274fbbcc901c4f
Isaias Santos <95.isaiassantos@gmail.com>
2021-02-11 18:51:00 -0300

    Pagina de filtro

---
+++ widget/filtro.php
@@ -0,0 +1,149 @@
+<div class="main_filter">
+    <div class="container">
+        <section class="row">
+            <div class="col-12">
+                <h2 class="text-front icon-filter">Filtro</h2>
+            </div>
+
+            <div class="col-12 col-md-12 col-lg-4">
+                <form action="" class="bg-white p-3 w-100 mb-5">
+                    <div class="row">
+                        <div class="form-group col-12">
+                            <label for="search" class="mb-2 text-front">Comprar ou alugar ?</label>
+                            <select name="search" id="search" class="selectpicker" title="Escolha..">
+                                <option value="">Comprar</option>
+                                <option value="">Alugar</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-12">
+                            <label for="search" class="mb-2 text-front">Comprar ou alugar ?</label>
+                            <select name="search" id="search" class="selectpicker" title="Escolha..">
+                                <option value="">Comprar</option>
+                                <option value="">Alugar</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-12">
+                            <label for="search" class="mb-2 text-front">Comprar ou alugar ?</label>
+                            <select name="search" id="search" class="selectpicker" title="Escolha.." multiple
+                                    data-actions-box="true">
+                                <option value="">Comprar</option>
+                                <option value="">Alugar</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-12">
+                            <label for="search" class="mb-2 text-front">Comprar ou alugar ?</label>
+                            <select name="search" id="search" class="selectpicker" title="Escolha..">
+                                <option value="">Comprar</option>
+                                <option value="">Alugar</option>
+                            </select>
+                        </div>
+                        <div class="col-12 mt-3 text-right">
+                            <button class="btn btn-front icon-search">Pesquisar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <div class="col-12 col-md-12 col-lg-8">
+                <section class="main_properties">
+                    <div class="container">
+                        <div class="row">
+                            <article class="col-12 col-md-12 col-lg-6">
+                                <div class="card main_properties_item">
+                                    <a href="">
+                                        <img class="card-img-top" src="properties/1/5a3571ab-4d76-466f-8246-eff8cb98cedd.jpg" alt="">
+                                    </a>
+                                    <div class="card-body">
+                                        <h2><a href="" class="text-front">Linda Casa no Campeche com vista para o Mar</a></h2>
+                                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i>
+                                        </p>
+                                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                                        <a href="" class="btn btn-front btn-block">Ver Imóvel</a>
+                                    </div>
+                                    <div class="card-footer d-flex text-muted">
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/bed.png" alt="" class="img-fluid">
+                                            <p class="text-muted">1</p>
+                                        </div>
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/garage.png" alt="" class="img-fluid">
+                                            <p class="text-muted">4</p>
+                                        </div>
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/util-area.png" alt="" class="img-fluid">
+                                            <p class="text-muted">180m²</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+
+                            <article class="col-12 col-md-12 col-lg-6">
+                                <div class="card main_properties_item">
+                                    <a href="">
+                                        <img class="card-img-top" src="properties/1/6abf463b-67a0-4fee-847e-42537699ad8e.jpg" alt="">
+                                    </a>
+                                    <div class="card-body">
+                                        <h2><a href="" class="text-front">Linda Casa no Campeche com vista para o Mar</a></h2>
+                                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i>
+                                        </p>
+                                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                                        <a href="" class="btn btn-front btn-block">Ver Imóvel</a>
+                                    </div>
+                                    <div class="card-footer d-flex text-muted">
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/bed.png" alt="" class="img-fluid">
+                                            <p class="text-muted">1</p>
+                                        </div>
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/garage.png" alt="" class="img-fluid">
+                                            <p class="text-muted">4</p>
+                                        </div>
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/util-area.png" alt="" class="img-fluid">
+                                            <p class="text-muted">180m²</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+
+                            <article class="col-12 col-md-12 col-lg-6">
+                                <div class="card main_properties_item">
+                                    <a href="">
+                                        <img class="card-img-top" src="properties/1/8f317473-e7b0-4c6e-8198-2ebe44a21f11.jpg" alt="">
+                                    </a>
+                                    <div class="card-body">
+                                        <h2><a href="" class="text-front">Linda Casa no Campeche com vista para o Mar</a></h2>
+                                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i>
+                                        </p>
+                                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                                        <a href="" class="btn btn-front btn-block">Ver Imóvel</a>
+                                    </div>
+                                    <div class="card-footer d-flex text-muted">
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/bed.png" alt="" class="img-fluid">
+                                            <p class="text-muted">1</p>
+                                        </div>
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/garage.png" alt="" class="img-fluid">
+                                            <p class="text-muted">4</p>
+                                        </div>
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/util-area.png" alt="" class="img-fluid">
+                                            <p class="text-muted">180m²</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </section>
+    </div>
+</div>
