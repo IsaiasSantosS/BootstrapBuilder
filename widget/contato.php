c3379a70283e8b19f63c9290bd606eec82698c92
Isaias Santos <95.isaiassantos@gmail.com>
2021-02-20 11:56:42 -0300

    pagina de contatos

--- widget/contato.php
+++ widget/contato.php
@@ -1,1 +1,55 @@
-<?php
+<div class="main_contato py-5 bg-light text-center">
+    <div class="container">
+        <h1 class="text-front">Entre Contato com Conosco</h1>
+        <p class="mb-0">Quer Conversar com um corretor exclusivo e ter o atendimento diferenciado em buscar do seu imóvel dos sonhos.</p>
+        <p>Preenche o formulário abaixo e vamos lhe direcionar para alguém que entende a sua necessidade!</p>
+
+        <div class="row text-left">
+            <form action="">
+                <h2 class="icon-envelope text-black-50">Envie um e-mail</h2>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Insira seu nome">
+                </div>
+                <div class="form-group">
+                    <input type="email" class="form-control" placeholder="Insira seu melhor e-mail">
+                </div>
+                <div class="form-group">
+                    <input type="tel" class="form-control" placeholder="Insira seu telefone com DDD">
+                </div>
+                <div class="form-group">
+                    <textarea name="teste" rows="5" placeholder="Escrever uma mensagem..." class="form-control"></textarea>
+                </div>
+
+                <div class="form-group">
+                    <button class="btn btn-front">Enviar Contato</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="main_contact_types py-5">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-12 col-md-4">
+                <h2 class="icon-envelope">Por E-mail</h2>
+                <p>Nossos atendentes vão entrar em contato com você assim que possível.</p>
+                <p><a href="" class="text-front">cursos@upinsider.com.br</a></p>
+            </div>
+            <div class="col-12 col-md-4">
+                <h2 class="icon-phone">Por Telefone</h2>
+                <p>Estamos disponíveis no número abaixo no horário comercial</p>
+                <p class="text-front py-2">+55 (48) 3322-1234</p>
+            </div>
+            <div class="col-12 col-md-4">
+                <h2 class="icon-phone">Redes Sociais</h2>
+                <p>Fique por dentro de tudo o que a gente compartilha em nossas redes sociais.</p>
+                <p>
+                    <button class="btn btn-front icon-facebook icon-notext"></button>
+                    <button class="btn btn-front icon-twitter icon-notext"></button>
+                    <button class="btn btn-front icon-instagram icon-notext"></button>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
